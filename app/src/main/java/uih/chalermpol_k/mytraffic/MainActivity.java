18f902a1e0fd32d5de4a3a005eee12aa43dfca6f
GMakeRich@gmail.com <GMakeRich@gmail.com>
2015-12-21 16:40:34 +0700

    จบ Class วันแรก

--- app/src/main/java/uih/chalermpol_k/mytraffic/MainActivity.java
+++ app/src/main/java/uih/chalermpol_k/mytraffic/MainActivity.java
@@ -2,12 +2,32 @@ package uih.chalermpol_k.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //  Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //  Bind Widget
+        bindwidget();
+
+    }   // Main Method
+
+    private void bindwidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (button) findViewById(R.id.button);
     }
-}
+
+}   // Main Class
