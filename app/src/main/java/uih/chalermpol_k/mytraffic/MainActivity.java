ab771eddb97464522996b2948ff7a866b0bbf077
GMakeRich@gmail.com <GMakeRich@gmail.com>
2015-12-22 10:26:29 +0700

    buttonController เสร็จแล้ว

--- app/src/main/java/uih/chalermpol_k/mytraffic/MainActivity.java
+++ app/src/main/java/uih/chalermpol_k/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package uih.chalermpol_k.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -22,12 +26,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //  Bind Widget
         bindwidget();
+        //  Button Controller
+        buttonController();
 
     }   // Main Method
 
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                buttonMediaPlayer.start();
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/eCi3lo1JaQI"));// Call Media uTube
+                startActivity(objIntent);
+
+            } //Event
+        });
+    }
+
     private void bindwidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutmeButton = (button) findViewById(R.id.button);
+        aboutmeButton = (Button) findViewById(R.id.button);
     }
 
 }   // Main Class
